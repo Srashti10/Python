93cecbe3cd64857faa099ecc71b31a8d24c9f4d5
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-07-27 13:36:50 +0530

    changes

--- day3.py
+++ day3.py
@@ -1,3 +1,5 @@
+#Modules 
+#modules and libraries are installed by using the command (pip install module name) eg. pip install pandas
 import pandas
 import hashlib
 print('hi')
--- day4.py
+++ day4.py
@@ -1,3 +1,6 @@
+#First program
+
+print("Hello World")
 print("Hello World", 7)
 print(5)
 print("Bye")
