cc434ed42e189345aa0db41144bdbb4898ae79f7
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-07-27 13:23:21 +0530

    changes

---
+++ day5.py
@@ -0,0 +1,32 @@
+# Escape Sequence Character
+
+#(\n) is an escape sequence character for the new line.
+# (\"\") is an escape sequence character for giving double quotes in a string inside print function.
+print("Hey! I am a good girl. \nand this viewer is also a good boy/girl")
+# This code is important. Please don't delete this.
+# (# is used for give comments)
+# \n is used to go to a new line.
+print("This is a single line comment.") #Single line comment.
+print("Hello World.") #Printing Hello World.
+
+'''This is multi-line comment.
+   Triple single quotes or triple double quotes 
+   are used to give multi line comments in python.
+'''
+"""
+   This is multi-line comment.
+   Triple single quotes or triple double quotes 
+   are used to give multi line comments in python.
+"""
+
+# To give double quotes inside the string we write it inside (\" \")
+
+print("Hey I am a \"good girl\" and this viewer is also a good person. ")
+
+print("Hey", 6, 7, sep="~") # here ~ is used as seperator
+# (sep=" ") is used to give seperator between the characters.
+print("Hey", 6, 7, sep="  ") # here space is the seperator between the characters.
+#default seperator is space
+print("Hey", "It's", "me", end="009\n") # here 009 will be printed after the last character of the print statement.
+print("Srashti")
+# (end="" is used to give an ending character after the print statement)
