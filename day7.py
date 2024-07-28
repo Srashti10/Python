41910db2c2f1e1b1e1ae472a79153fb7610546bf
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-07-28 21:19:05 +0530

    changes

---
+++ day7.py
@@ -0,0 +1,10 @@
+# Operators 
+# 1- Arithmetic Operators
+
+print(15+7)  # Addition
+print(15-7)  # Subtraction
+print(15*7)  # Multiplication
+print(15/7)  # Division
+print(15**7) # Exponential(It gives the result of 15 to the power 7)
+print(15%7)  # Modulus(It gives the remainder)
+print(15//7) # Floor Division
