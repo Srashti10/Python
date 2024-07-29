6ed9a61326c2d0e8b2fc9f7b3fb47cc757697213
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-07-29 16:43:37 +0530

    changes

---
+++ day10.py
@@ -0,0 +1,15 @@
+# User Input
+# In python we can take user input directly by using input() function
+
+# a = input()
+# print("My name is", a)
+
+# a = input("Enter your name:")
+# print("My name is", a)
+
+x = input("Enter first number:")
+y = input("Enter second number:")
+print(x+y)
+
+# input function will always consider your input as string by default
+
