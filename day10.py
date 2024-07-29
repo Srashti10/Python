83c51805ff17b65b91927d840eeacc8201dc32a3
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-07-29 16:53:23 +0530

    Changes

--- day10.py
+++ day10.py
@@ -7,9 +7,16 @@
 # a = input("Enter your name:")
 # print("My name is", a)
 
-x = input("Enter first number:")
-y = input("Enter second number:")
-print(x+y)
+# x = input("Enter first number:")
+# y = input("Enter second number:")
+# print(x+y)
+# print(int(x) + int(y))
 
 # input function will always consider your input as string by default
+# for taking integer as input we will have to use type casting using int() function
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+print(x+y)
+
 
