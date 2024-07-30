64cdf97bbb2bd68efb808aa7eec81a4aede349a0
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-07-30 13:44:44 +0530

    changes

---
+++ day11.py
@@ -0,0 +1,31 @@
+# Strings: In python anything that enclose between single or double quotation marks is considered a string.
+# It is like an array of character.
+
+name = "Srashti"
+print("Hello", name)
+
+# To give double quotes inside the string we can use two methods.
+print("He said, \"I want to eat an apple\"")
+print('He said, "I want to eat an apple"')
+
+# Multiline strings
+
+conversation = """Hi there!
+Good morning.
+I am looking for a job.
+Here is my resume"""
+
+print(conversation)
+
+# Indexing
+firstName = "Srashti"
+print(firstName)
+print(firstName[0])
+print(firstName[1])
+print(firstName[2])
+print(firstName[3])
+print(firstName[4])
+print(firstName[5])
+print(firstName[6])
+# print(firstName[7]) This throws an error
+ 
