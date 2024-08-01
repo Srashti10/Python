e5f1806931adddfaec05dc9e8986dc10035eab4c
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-08-01 11:44:16 +0530

    changes

---
+++ day15.py
@@ -0,0 +1,6 @@
+# Exercise 2  (Good Morning Sir!)
+# We have to make a program to greet a person according to the time.
+
+import time   # This function is used to use time in the program
+timestamp = time.strftime("%H,%M,%S")   #strftime stands for string format time, It is used to display time in string format.
+print(timestamp)
