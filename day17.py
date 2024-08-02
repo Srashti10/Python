e92559e8d5143a4eda08cf990c6856324b343f7d
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-08-02 11:02:39 +0530

    changes

---
+++ day17.py
@@ -0,0 +1,22 @@
+# For loops
+# name = "Abhishek"
+# for i in name:
+#     print(i)
+#     if(i=="b"):
+#         print("This is something special.")
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# range()
+# It has three parameters: start, stop and step
+
+for i in range(5,10):  # It will print numbers from 5 to 9
+    print(i)
+
+for i in range(1,20,2): # It will print numbers 1,3,5,7,9,11,13,15,17,19
+    print(i)
+
