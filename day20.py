b5932fcec422c174490ea9c4e3e15754524b2db1
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-08-03 22:55:22 +0530

    changes

--- day20.py
+++ day20.py
@@ -1,7 +1,10 @@
 # Functions
 # If we want to repeat the logic of code multiple times in our program, we have to repeat it multiple times.
 # But we don't want to repeat specific part multiple times because , because it is very difficult to update in multiple times.
-# But with the help of functions we can make that ortion of code a different entity an dthen use it multiple places in the program.
+# But with the help of functions we can make that portion of code a different entity anduse it multiple places in the program.
+# Functions are of two types: 1- Built-in function 2- User-defined functions  
+# In built-in function we don't have to use the def keyword. some examples of built-in functions are: range(), print(),type().
+# Rules of naming the functions are same as variables.
 # Geometric Mean = (a*b)/(a+b)
 # How to create a function to create geometric mean.
 def calculateGmean(a,b):    # calculateGmean = name of the function, (a,b) = arguments of the function
@@ -17,7 +20,10 @@ def isGreater(a,b):
     else:
         print("Both numbers are equal.")
 
-
+# If we want to declare a function now and wanted to add function body later, then we can use pass statement to tell python
+# interpreter that you will add the function body later. If you leave it without giving pass, then it will show error.
+def isLesser(a,b):
+    pass  # pass means go to the next line to execute
 
 
 calculateGmean(10,20)    # calling of the function calculateGmean
