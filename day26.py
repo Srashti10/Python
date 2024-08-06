533af134c089e63de6011a74bceb768091e82ad0
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-08-06 10:17:25 +0530

    changes

---
+++ day26.py
@@ -0,0 +1,15 @@
+# Exercise2 Solution  (Good Morning Sir)
+# We have to make a program to greet a person according to the time.
+# "Harry's Solution"
+
+import time 
+t = time.strftime('%H:%M:%S')
+hour = int(time.strftime("%H"))
+print(hour)
+
+if(hour>=0 and hour<12):
+    print("Good Morning Sir!")
+elif(hour>12 and hour<17):
+    print("Good Afternoon Sir")
+elif(hour>=17 and hour<0):
+    print("Good Night Sir")
