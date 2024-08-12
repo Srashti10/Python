2c7828949e6538308e9bda0722795a640cb4b844
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-08-12 21:29:25 +0530

    changes

--- day27.py
+++ day27.py
@@ -3,10 +3,13 @@
 # Use list data type to store questions and their correct answers.
 # Display the final amount the person is taking home after playing the game.
 # My Solution
-
-quesions = [" What comes after 50?"]
+print("Welcome to Kaun Banega Crorepati")
+question = [" What comes after 50?"]
 answers = [51,52,53,54]
-yourAns = input("Enter your answers:")
+print(question)
+print("Your options are:","\n",answers[0] ,"\n",answers[1],"\n",answers[2],"\n",answers[3])
+print("Entter your answer:")
+yourAns = int(input())
 if yourAns == answers[0]:
     print("Correct Answer! You win Rs.1000")
 else:
