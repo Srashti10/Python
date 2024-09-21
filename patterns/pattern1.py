f7a50ba3258df250dbc380d13ad0607035d25046
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-09-21 15:46:39 +0530

    changes

--- patterns/pattern1.py
+++ patterns/pattern1.py
@@ -1,11 +1,13 @@
+# Right angle triangle pattern with stars
+
 num = int(input('Enter the number of rows:'))
 for i in range(1,num+1):   
     for j in range(1,i+1):
         print('*',end=' ')
     print()   #It will print the new line
 
-num1 = int(input('Enter the number:'))
-for i in range(1,num1+1):
-    for j in range(1,i+1):
-        print(j,end=' ')
-    print()
+# num1 = int(input('Enter the number:'))
+# for i in range(1,num1+1):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
