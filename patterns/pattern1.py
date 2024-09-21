24af744762ed733708969a34db2ad008822dd9d7
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-09-21 11:27:23 +0530

    changes

--- patterns/pattern1.py
+++ patterns/pattern1.py
@@ -1,5 +1,11 @@
 num = int(input('Enter the number of rows:'))
-for i in range(1,num+1):
+for i in range(1,num+1):   
     for j in range(1,i+1):
         print('*',end=' ')
+    print()   #It will print the new line
+
+num1 = int(input('Enter the number:'))
+for i in range(1,num1+1):
+    for j in range(1,i+1):
+        print(j,end=' ')
     print()
