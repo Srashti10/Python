feee2b2d5860d3be7615b1c833d412f30c2f0db7
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-09-23 20:37:09 +0530

    changes

--- patterns/pattern3.py
+++ patterns/pattern3.py
@@ -1,34 +1,9 @@
-# # Pyramid based pattern
-# num = int(input('Enter the number of rows:'))
-# for i in range(0,num):
-#     for i in range(0,num-i-1):
-#         print(end="")
-#     for i in range(0,num-i):
-#         print('*',end="")
-#     print()
-
-# num = int(input('Enter the number of rows:'))
-# for i in range(1,num+1):
-#     for j in range(1,i+1):
-#         print('*',end='')
-#     print()
-
-# num = int(input('Enter the number of rows:'))
-# for i in range(1,num+1):
-#     for j in range(1,i+1):
-#         print(i,end='')
-#     print()
-
-# num = int(input('Enter the number of rows:'))
-# for i in range(1,num+1):
-#     for j in range(1,i+1):
-#         print(j,end='')
-#     print()
-
+# Pyramid based pattern
 num = int(input('Enter the number of rows:'))
-k = 1
-for i in range(1,num+1):
-    for j in range(1,k+1):
-        print('*',end='')
-    k = k+2
+for i in range(0,num):
+    for i in range(0,num-i-1):
+        print(end="")
+    for i in range(0,num-i):
+        print('*',end="")
     print()
+
