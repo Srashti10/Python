ffdfc5cf3402a80840e42b0d7c494cb2bdca20d7
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-09-24 12:44:33 +0530

    changes

--- patterns/pattern3.py
+++ patterns/pattern3.py
@@ -7,3 +7,10 @@ for i in range(0,num):
         print('*',end=" ")
     print()
 
+num = int(input('Enter the number of rows:'))
+for i in range(0,num):
+    for j in range(0,num-i-1):
+        print(end=" ")
+    for j in range(0,i+1):
+        print('*',end="")
+
