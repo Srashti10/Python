8eb451f2439f8116e8afc6ef593540810786bb2a
Srashti Srivastava <srivastavasrashti45@gmail.com>
2024-09-26 19:17:02 +0530

    changes

--- interview-coding/remove_duplicate.py
+++ interview-coding/remove_duplicate.py
@@ -3,10 +3,10 @@
 # print(temp)
 
 list = [1,1,6,6,6,2,2,2,5,3,3]
-a = list.sort()
-n = len(a)
-for i in a:
-    if a[i] == a[i+1]:
-        list.remove(a[i+1])
+app = list.sort()
+n = len(app)
+for i in app:
+    if app[i] == app[i+1]:
+        list.remove(app[i+1])
 
 print(a)
